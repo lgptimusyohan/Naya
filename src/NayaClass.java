8d606c1c669098fe380bc7f041b72a2a1335be9e
Yohan Wadia <persianpowerman@hotmail.com>
2019-10-11 17:07:40 +0300

    from Dell

--- src/NayaClass.java
+++ src/NayaClass.java
@@ -3,6 +3,7 @@ public class NayaClass {
     public static void main(String[] args) {
         String str1 = "from Dell with lgOptimus";
 
+        String str2 = "from Dell but now in DellsWorking branch";
     }
 
 }
